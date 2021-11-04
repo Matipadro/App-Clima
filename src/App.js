37a0be70a145e29fab79f9587f800dc65500ea7d
Matipadro <matipadro@gmail.com>
2021-11-04 12:18:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Cards from './components/Cards.jsx';
 import SearchBar from './components/SearchBar.jsx';
+import data from './data.js';
 
 function App() {
   return (
